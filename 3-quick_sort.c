ee7e061099e634eeb43f6d73ab74434f0c22735c
AdeleM-prog <12043@holbertonstudents.com>
2025-12-10 14:33:48 +0000

    travali en cours T3

---
+++ 3-quick_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+ void quick_sort(int *array, size_t size)
+ {
+    size_t i, j, pivot;
+
+    if (array == NULL || size < 2)
+    return;
+
+
+ }
