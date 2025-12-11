6368bf81eda8bd15d90d883cda5b8f45125cbbb5
AdeleM-prog <12043@holbertonstudents.com>
2025-12-11 10:44:35 +0000

    correction checker 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,14 +7,12 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int tmp = 0;
+size_t i, j, tmp = 0;
 int flag;
 if (array == NULL || size == 0)
 return;
 for (j = 0; j < (size - 1); j++)
 {
-i = 0;
 flag = 0;
 for (i = 0; i < size - j - 1; i++)
 {
@@ -23,8 +21,9 @@ if (array[i] > array[i + 1])
 tmp = array[i];
 array[i] = array[i + 1];
 array[i + 1] = tmp;
-print_array(array, size);
 flag = 1;
+print_array(array, size);
+
 }
 }
 if (flag == 0)
