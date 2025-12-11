947ea5dafb0b2692562e29eea2beb80848cb5f2e
Melopheelo12 <ryan12052002@gmail.com>
2025-12-11 09:32:49 +0000

    Modification

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,40 +1,39 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- * using the Insertion sort algorithm.
+ * insertion_sort_list - Sorts a doubly linked list of integers in
+ * ascending order using the Insertion sort algorithm.
  *
  * @list: The doubly linked list to be sorted.
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *insert, *temp;
+listint_t *current, *insert, *temp;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+if (!list || !(*list) || !(*list)->next)
+return;
 
-    current = (*list)->next;
-    while (current)
-    {
-        insert = current;
-        while (insert->prev && insert->n < insert->prev->n)
-        {
-            temp = insert->prev;
-            if (temp->prev)
-                temp->prev->next = insert;
-            else
-                *list = insert;
+current = (*list)->next;
+while (current)
+{
+insert = current;
+while (insert->prev && insert->n < insert->prev->n)
+{
+temp = insert->prev;
+if (temp->prev)
+temp->prev->next = insert;
+else
+*list = insert;
 
-            insert->prev = temp->prev;
-            temp->next = insert->next;
-            if (insert->next)
-                insert->next->prev = temp;
-            insert->next = temp;
-            temp->prev = insert;
+insert->prev = temp->prev;
+temp->next = insert->next;
+if (insert->next)
+insert->next->prev = temp;
+insert->next = temp;
+temp->prev = insert;
 
-            print_list(*list);
-        }
-        current = current->next;
-    }
+print_list(*list);
 }
- 
+current = current->next;
+}
+}
